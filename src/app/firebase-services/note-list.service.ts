239386157e7b07d946955ec21e153a308b72a081
elidegen <elidegen@icloud.com>
2023-10-09 20:27:29 +0200

    x

---
+++ src/app/firebase-services/note-list.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteListService {
+
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubTrash;
+  unsubNotes;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+  }
+
+  ngOnDestroy() {
+    this.unsubTrash();
+    this.unsubNotes();
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    console.log('setNoteObject', obj);
+    return {
+      id: id,
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false
+    }
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+        console.log('subtrashlist', this.trashNotes);
+      });
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'danotes-e6906',
+    appId: '1:1098439028786:web:c61baf5d2772c5a2a2cc2b',
+    storageBucket: 'danotes-e6906.appspot.com',
+    apiKey: 'AIzaSyBshMhk0exlX9DjwPjrjnAKI13g1Zg3KgY',
+    authDomain: 'danotes-e6906.firebaseapp.com',
+    messagingSenderId: '1098439028786',
+  },
+};
